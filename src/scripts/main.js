de00dba9fc2656cb870f347f0591e068ea0124f9
Maxwell <110508088+mxwlladriano@users.noreply.github.com>
2024-02-21 12:18:07 -0300

    Add files via upload

---
+++ src/scripts/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+
+
+    for (let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+})
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+   const elementoPai = elemento.target.parentNode;
+
+   elementoPai.classList.toggle(classe);
+}
