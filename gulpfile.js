bbfb692265bdff603d6c63450bc0b30c319ba47c
mxwlladriano <mxwlladriano@gmail.com>
2024-02-21 21:06:57 -0300

    inclusão dos links de redes sociais

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,19 @@ const imagemin = require('gulp-imagemin');
 
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
+    return gulp.src('./portfolio/src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./portfolio/dist/css'));
 }
 
 function images() {
-    return gulp.src('./src/images/**/*')
+    return gulp.src('./portfolio/src/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+        .pipe(gulp.dest('./portfolio/dist/images'));
 }
 
 
 exports.default = gulp.parallel(styles, images);
 exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./portfolio/src/styles/*.scss', gulp.parallel(styles))
 }
